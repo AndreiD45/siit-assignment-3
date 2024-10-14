6c1d81716b467e72c48faae218c3238f185cd409
Dolean Andrei <forandrei@yahoo.com>
2024-10-14 15:11:38 +0300

    finnaly finished exercise 2. I kept trying in vain until i discovered that we could use and then it was easy

--- exercise-02/main.js
+++ exercise-02/main.js
@@ -1,68 +1,59 @@
 
-// function computerOption(){
-//     const options = [`rock`, 'paper', 'scissors'];
-//     const randomIndex = Math.floor(Math.random() * options.length);
-//     return options[randomIndex];
-// }
 
-// function newRound(){
-//     console.log('');
-//     console.log(`NEW ROUND!`);
+const options = ['rock', 'paper', 'scissors'];
+function computerOption(){
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
+
+
+function newRound(){
+    console.log('');
+    console.log(`NEW ROUND!`);
     
-//     const uChoice = prompt(`Input your choice`);
+    const uChoice = prompt(`Input your choice`);
 
-//     const validInput = true;
+    let validInput;
+    validInput = validity(uChoice);
 
-//     if(validInput){
-//         const cChoice = computerOption();
-//         const result = detectWinner(uChoice, cChoice);
-//         console.log(result);
-//     }
-//     else{
-//         console.error(`Invalid input: ${uChoice}`);
-//     }
-// }
-
-
-// function detectWinner(userChoice, computerChoice){
-//     console.log(`User's choice: ${userChoice}`);
-//     console.log(`Computers's choice: ${computerChoice}`);
-
-// let computerChoice;
-// let userChoice;
-// let result;
-// let options = [0, 1, 2];
-
-function getResult(uChoice, cChoice){
-    if(uChoice == cChoice){
-        console.log(uChoice);
-        console.log(cChoice);
-        return `You tied`;
-    }else if(uChoice == 1, cChoice == 0){
-        console.log(uChoice);
-        console.log(cChoice);
-        return `You won!`;
-    }else if(uChoice == 2, cChoice == 1){
-        console.log(uChoice);
-        console.log(cChoice);
-        return `You won!`;
-    }else if(uChoice == 0, cChoice == 2){
-        console.log(uChoice);
-        console.log(cChoice);
-        return `You won!`;
-    }else if(uChoice == 0, cChoice == 1){
-        console.log(uChoice);
-        console.log(cChoice);
-        return `You lost!`;
-    }else if(uChoice == 1, cChoice == 2){
-        console.log(uChoice);
-        console.log(cChoice);
-        return `You lost!`;
-    }else if(uChoice == 2, cChoice == 0){
-        console.log(uChoice);
-        console.log(cChoice);
-        return `You lost!`;
+    if(validInput === `1`){
+        const cChoice = computerOption();
+        const result = detectWinner(uChoice, cChoice);
+        console.log(result);
+    }
+    else{
+        console.error(`Invalid input: ${uChoice}`);
     }
 }
 
 
+function detectWinner(userChoice, computerChoice){
+    console.log(`User's choice: ${userChoice}`);
+    console.log(`Computers's choice: ${computerChoice}`);
+    return result = getResult(userChoice, computerChoice);
+}
+
+function getResult(a, b){
+    if(a === b){
+        return `Its a tie!`
+    }else if(a === `rock` && b === `paper`){
+        return  `You lost!`
+    }else if(a === `paper` && b === `scissors`){
+        return `You lost!`
+    }else if(a === `scissors` && b === `rock` ){
+        return `You lost`
+    }else{
+        return `You won!`
+    }
+}
+
+function validity(a){
+    if(a ==`rock`){
+        return `1`;
+    }else if(a == `paper`){
+        return `1`;
+    }else if(a == `scissors`){
+        return `1`;
+    }else
+        return `0`;
+}
